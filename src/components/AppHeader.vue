e55a1bed183a7a42c35e106c81ebe7390dea21ea
AlenaNik <alena.nik.digital@gmail.com>
2020-07-11 12:25:55 +0200

    button test

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+            <button v-show="loggedIn">Logout</button>
+<!--            <button @click="loggedIn = !loggedIn">Toggle button</button>-->
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                loggedIn: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
